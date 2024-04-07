8f5c7093193b998cba96e7df4b86bc06b4e18d3d
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2024-04-08 00:54:07 +0900

    Fix: NodeモジュールをVite内で呼び出せるように変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+    // TODO: ↓依存しない形式に変更できないかを調査して
+    nodePolyfills(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
