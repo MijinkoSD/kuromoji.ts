a08b4efe69b736c359eed47590c1d0f5c75d8b4e
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2024-05-05 01:58:56 +0900

    Update: デモ画面を作成

--- demo/App.vue
+++ demo/App.vue
@@ -3,147 +3,183 @@ import Tokenizer from "../src/Tokenizer";
 import kuromoji from "../src/kuromoji";
 import { ref, onMounted } from "vue";
 import { IpadicFormatterToken } from "../src/util/IpadicFormatter";
+import ResultTable from "./components/ResultTable.vue";
 
 const DIC_URL = "/";
 
 const inputText = ref("");
 const tokens = ref<IpadicFormatterToken[]>([]);
 const svgStyle = ref("");
-const message = ref("");
-const isLoading = ref(false);
+const messageBeforeLoaded = ref("ロード中です。");
+const isLoading = ref(true);
 let tokenizer: Tokenizer | undefined = undefined;
 
 const tokenize = () => {
   if (inputText.value == "" || tokenizer === undefined) {
     tokens.value = [];
-    // lattice = null;
     return;
   }
   try {
-    // lattice = tokenizer.getLattice(inputText);
     tokens.value = tokenizer.tokenize(inputText.value);
   } catch (e) {
-    console.log(e);
-    // lattice = null;
+    console.error(e);
     tokens.value = [];
   }
 };
 
 // フォームの内容が変化したらtokenizeする
 const onChangeInput = () => {
-  // vm.graphEnabled = false;
   svgStyle.value = "hidden";
   tokenize();
 };
 
-onMounted(() => {
+onMounted(async () => {
   // Load and prepare tokenizer
-  kuromoji.builder({ dicPath: DIC_URL }).buildBrowser((errors, _tokenizer) => {
-    if (errors.find((e) => e !== null)) {
-      for (const error of errors) {
-        if (error === null) continue;
-        console.error(error);
+  // tokeniserの生成・読み込み
+  await kuromoji
+    .builder({ dicPath: DIC_URL })
+    .buildBrowser((errors, _tokenizer) => {
+      if (errors.find((e) => e !== null)) {
+        for (const error of errors) {
+          if (error === null) continue;
+          console.error(error);
+        }
       }
-    }
-    tokenizer = _tokenizer;
+      tokenizer = _tokenizer;
 
-    message.value = "Ready";
-
-    inputText.value = "すもももももももものうち";
-    isLoading.value = false;
-  });
+      inputText.value = "すもももももももものうち";
+      isLoading.value = false;
+    });
+  tokenize();
 });
 </script>
 
 <template>
-  <div class="row">
-    <div class="small-12 columns">
-      <h1>kuromoji.ts demo</h1>
+  <div id="app">
+    <header>
+      <span class="title">kuromoji<span class="accent">.ts</span> demo</span>
+    </header>
 
-      <div id="demo" class="row">
-        <div class="large-12 columns">
-          <div v-show="isLoading" data-alert="" class="alert-box radius">
-            <div v-text="message"></div>
-          </div>
-
-          <form v-show="!isLoading">
-            <label
-              >解析対象
-              <input
-                type="text"
-                placeholder="解析したい文字列を入力してください"
-                v-model="inputText"
-                @input="onChangeInput"
-              />
-              <input type="text" name="dummy" style="display: none" />
-            </label>
-
-            <table v-show="tokens.length" width="100%">
-              <thead>
-                <tr>
-                  <th>表層形</th>
-                  <th>品詞</th>
-                  <th>品詞細分類1</th>
-                  <th>品詞細分類2</th>
-                  <th>品詞細分類3</th>
-                  <th>活用型</th>
-                  <th>活用形</th>
-                  <th>基本形</th>
-                  <th>読み</th>
-                  <th>発音</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="token in tokens">
-                  <td>{{ token.surface_form }}</td>
-                  <td>{{ token.pos }}</td>
-                  <td>{{ token.pos_detail_1 }}</td>
-                  <td>{{ token.pos_detail_2 }}</td>
-                  <td>{{ token.pos_detail_3 }}</td>
-                  <td>{{ token.conjugated_type }}</td>
-                  <td>{{ token.conjugated_form }}</td>
-                  <td>{{ token.basic_form }}</td>
-                  <td>{{ token.reading }}</td>
-                  <td>{{ token.pronunciation }}</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <!--
-                    <a href="#" v-on="click: drawGraph" v-show="svgStyle=='hidden'" class="radius button">ラティスを表示する</a>
-                    -->
-
-            <!-- <svg style="{ visibility: svgStyle }" width="100%" height="800px">
-              <g id="lattice" transform="translate(20,20)"></g>
-            </svg> -->
-          </form>
+    <main>
+      <div v-show="isLoading" data-alert="" class="alert-box radius">
+        <div class="text-area">
+          <p>{{ messageBeforeLoaded }}</p>
         </div>
       </div>
-    </div>
+
+      <div v-show="!isLoading" class="demo">
+        <div class="input">
+          <label class="label">解析対象</label>
+          <div class="input-area">
+            <input
+              type="text"
+              placeholder="解析したい文字列を入力してください"
+              v-model="inputText"
+              @input="onChangeInput"
+            />
+            <div class="outline" />
+          </div>
+        </div>
+
+        <ResultTable :tokens="tokens"></ResultTable>
+      </div>
+    </main>
   </div>
 </template>
 
 <style scoped>
-text {
-  font-weight: 300;
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 12px;
+#app {
+  --color-1: hsl(211 84% 50% / 1);
+  --color-w: hsl(80 50% 90% / 1);
+  --color-2: hsl(51 84% 50% / 1);
 }
 
-.node rect {
-  stroke: #999;
-  stroke-width: 1px;
-  fill: #fff;
+header {
+  margin: 0.5em 0 1em;
+  padding: 0 1em;
+  /* box-shadow: 0px 3px 1px -1px hsl(160 0% 80% / 1); */
 }
 
-.edgeLabel rect {
-  fill: #fff;
+.title {
+  padding-bottom: 0.1em;
+  display: inline-block;
+  width: 100%;
+  letter-spacing: -1px;
+  font-size: 2.5em;
+  font-family: "Nunito Sans", sans-serif;
+  font-weight: 700;
+
+  * {
+    font-family: "Nunito Sans", sans-serif;
+  }
+
+  .accent {
+    font-weight: 800;
+    background: linear-gradient(
+      120deg,
+      var(--color-1) 48%,
+      color-mix(in srgb-linear, var(--color-1), var(--color-w)) 53%,
+      color-mix(in srgb-linear, var(--color-w), var(--color-2)) 62%,
+      var(--color-2) 67%
+    );
+    background-clip: text;
+    color: transparent;
+  }
 }
 
-.edgePath path {
-  stroke: #333;
-  stroke-width: 1.5px;
-  fill: none;
+main {
+  padding: 0 2em;
+}
+
+.demo {
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+
+  .label {
+    font-size: 0.9em;
+  }
+
+  .input-area {
+    /* width: 100%;
+    height: fit-content; */
+    padding-left: 0.5em;
+  }
+
+  input {
+    border: none;
+    padding: 0;
+    outline: none;
+    width: 100%;
+    font-size: 1.2em;
+    font-weight: 600;
+    border-image: linear-gradient(90deg, #ccc, #ccc) 1;
+  }
+
+  .outline {
+    margin-right: auto;
+    border-bottom: 2px solid #ccc;
+    display: block;
+    width: 100%;
+    height: 0;
+    border-radius: 3px;
+
+    &::after {
+      content: "";
+      border-bottom: 2px solid hsl(211 77% 65% / 1);
+      display: block;
+      width: 0%;
+      transition: all 0.3s 0s ease;
+    }
+  }
+
+  input:focus-visible ~ .outline::after {
+    width: 100%;
+  }
 }
 </style>
-../src/Tokenizer.ts../src/kuromoji.ts../src/util/IpadicFormatter.ts
---
+++ demo/components/ResultTable.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { IpadicFormatterToken } from "../../src/util/IpadicFormatter";
+
+defineProps<{
+  tokens: IpadicFormatterToken[];
+}>();
+</script>
+
+<template>
+  <table v-show="tokens.length" class="result">
+    <thead>
+      <tr>
+        <th>表層形</th>
+        <th>品詞</th>
+        <th>品詞細分類1</th>
+        <th>品詞細分類2</th>
+        <th>品詞細分類3</th>
+        <th>活用型</th>
+        <th>活用形</th>
+        <th>基本形</th>
+        <th>読み</th>
+        <th>発音</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="token in tokens">
+        <td>{{ token.surface_form }}</td>
+        <td>{{ token.pos }}</td>
+        <td>{{ token.pos_detail_1 }}</td>
+        <td>{{ token.pos_detail_2 }}</td>
+        <td>{{ token.pos_detail_3 }}</td>
+        <td>{{ token.conjugated_type }}</td>
+        <td>{{ token.conjugated_form }}</td>
+        <td>{{ token.basic_form }}</td>
+        <td>{{ token.reading }}</td>
+        <td>{{ token.pronunciation }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+.result {
+  box-sizing: content-box;
+  border: 1px solid transparent;
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 1200px;
+  border-radius: 3px;
+  overflow: hidden;
+}
+
+thead {
+  background-color: hsl(0 0% 55% / 1);
+  color: white;
+
+  th {
+    font-weight: normal;
+  }
+}
+
+tbody tr:nth-child(even) {
+  background-color: hsl(0 0% 90% / 1);
+}
+
+th,
+td {
+  border: 1px solid hsl(0 0% 80% / 1);
+  padding: 0.25em;
+}
+</style>
