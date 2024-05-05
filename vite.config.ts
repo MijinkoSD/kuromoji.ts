566c0e3613f756581899832f74605c57759862f9
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2024-05-06 02:35:53 +0900

    Update: 辞書データ生成のコマンドとDemoサイトビルド設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-
+  build: {
+    outDir: "./dist-demo",
+  },
   publicDir: "./dict",
 });
